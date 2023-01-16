00414c20f2cc9cb8a3b65f5c34bc0e997e028a29
khoale <kthedestroy@gmail.com>
2023-01-16 23:01:58 +0700

    remove scroll and all other event

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    async rewrites() {
+        return [
+            {
+                source: "/iframe/:path*",
+                destination: "http://chaythienngo.vn/:path*",
+            },
+            {
+                source: "/mobile/:path*",
+                destination: "http://chaythienngo.vn/mobile/:path*",
+            },
+            {
+                source: "/files/:path*",
+                destination: "http://chaythienngo.vn/files/:path*",
+            },
+        ];
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
